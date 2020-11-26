b81d8b8799331552e3a8ebbd4ec36608098c569b
nrcool <nrcoolove@yahoo.com>
2020-11-26 10:01:45 +0100

    Updates

---
+++ precache-manifest.79b2951c99986717c7a0ec0eba6526d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc64088b0df6afffd932a57958e0e4c9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "667aa5ba684ad29900ba",
+    "url": "./static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "667aa5ba684ad29900ba",
+    "url": "./static/js/2.b6d430a9.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./static/js/2.b6d430a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd3ff140949b45cbe21e",
+    "url": "./static/js/main.9bd07aa7.chunk.js"
+  },
+  {
+    "revision": "8cdb25f3ffb41440c4d8",
+    "url": "./static/js/runtime-main.e6c4d886.js"
+  }
+]);
